46d24e0fe1294e615761e0611afd141432e9408f
Rafael Guillén <arthexis@gmail.com>
2022-11-18 00:44:52 -0600

    Upkeep commit

--- qaczar.py
+++ qaczar.py
@@ -157,30 +157,10 @@ def first_load():
     RUNLEVEL = 3
 
 
-# Calculate levenstein distance between two strings.
-def levenshtein(s1, s2):
-    if len(s1) < len(s2):
-        return levenshtein(s2, s1)
-    if len(s2) == 0:
-        return len(s1)
-    previous_row = range(len(s2) + 1)
-    for i, c1 in enumerate(s1):
-        current_row = [i + 1]
-        for j, c2 in enumerate(s2):
-            insertions = previous_row[j + 1] + 1
-            deletions = current_row[j] + 1
-            substitutions = previous_row[j] + (c1 != c2)
-            current_row.append(min(insertions, deletions, substitutions))
-        previous_row = current_row
-    return previous_row[-1]
-
-
 # Process an incoming request.
 def process_request(request):
-    # Get the name of all the functions in the global namespace.
-    functions = [f for f in globals() if callable(globals()[f])]
-    # Calculate the levenshtein distance between the request and each function.
-    distances = [(f, levenshtein(request, f)) for f in functions]
+    log(f'Processing request: {request}')
+    return 'Success'
 
 
 # Generate a checklist of all the todos.
