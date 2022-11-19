a20fc00313e5ed76c742fe9dd0274834ce6b2cf5
Rafael Guillén <arthexis@gmail.com>
2022-11-19 02:54:05 -0600

    Backup <source> #17

--- qaczar.py
+++ qaczar.py
@@ -13,7 +13,6 @@
 import os
 import sys
 import time
-import html
 import random
 import sqlite3
 import subprocess
@@ -197,7 +196,7 @@ HOST = os.environ.get('HOST', 'localhost')
 PORT = int(os.environ.get('PORT', 8080))
 
 def facade_app(environ, start_response):
-    source = html.escape(last('source')[1])
+    source = last('source')[1]
     body = f'''
         <!DOCTYPE html>
         <meta http-equiv="refresh" content="{heartbeat()}">
