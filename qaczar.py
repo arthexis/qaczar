8a83a40555efbd2a74cfaee3a67e90b0d9b3b814
Rafael Guillén <arthexis@gmail.com>
2022-11-21 22:00:35 -0600

    better styling

--- qaczar.py
+++ qaczar.py
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*-
 
 # A python script that implements observable goal-oriented self-sufficiency.
+# It runs every aspect of the web site you are currently visiting.
+# The code you are reading is the code that is running at all times.
 # H. V. D. C. by Rafa Guillén (arthexis@github) 2022
 
 
-# TODO: More testing on the rollback: always write plan to disk at least once.
-# TODO: Add visitors with goal-oriented behavior.
+# TODO: Figure out how to recast the crown from a delegate.
+# TODO: Figure out a cool way to do data-entry.
+# TODO: Check permissions automatically based on user (consider SSL).
 # TODO: Achieve ascension.
 
 import re
@@ -17,17 +20,18 @@ import atexit
 import subprocess
 
 
-QACZAR = 'QACZAR'
+SITE = 'QACZAR.COM'
 RUNLEVEL = len(sys.argv)
 EPOCH = time.time()
+DIR = os.path.dirname(__file__)
 
 with open(__file__, 'r', encoding='utf-8') as f:
     BODY = f.read()
 assert BODY
 
 
+# All logging is in this format and redirected to stdout.
 def emit(verse):
-    # All logging is in this format and redirected to stdout.
     print(f'[{RUNLEVEL}:{sys._getframe(1).f_lineno}:{awakened()}] {verse}')
 
 
@@ -35,7 +39,7 @@ def awakened():
     return round(time.time() - EPOCH, 4)
 
 
-def isotime():
+def iso8601():
     return time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())
 
 
@@ -117,7 +121,7 @@ def palace_recall(topic, /, fetch='last', store=None):
     if not TOPICS:
         c.execute('SELECT name FROM sqlite_master WHERE type="table" AND name not LIKE "sqlite_%"')
         TOPICS.extend(t[0] for t in c.fetchall())
-    if topic not in TOPICS:
+    if topic not in TOPICS and store:
         c.execute(
             f'CREATE TABLE IF NOT EXISTS {topic} ('
             f'num INTEGER PRIMARY KEY AUTOINCREMENT, '
@@ -136,12 +140,13 @@ def palace_recall(topic, /, fetch='last', store=None):
         store = r.fetchone()[2]
     if store is not None: 
         rowid = c.execute(f'INSERT INTO {topic} (ts, artifact) VALUES (?, ?)',
-                  (isotime(), store)).lastrowid
+                  (iso8601(), store)).lastrowid
         if fetch == 'new':
             r = c.execute(f'SELECT * FROM {topic} WHERE num = ?', (rowid,))
         emit(f'Stored {topic=} {rowid=} {len(store)=}.')
         PALACE.commit()
     return r.fetchall()
+        
 
 
 def dynamic_fetch(c, topic, fetch):
@@ -150,13 +155,17 @@ def dynamic_fetch(c, topic, fetch):
 
 # V.
 
+import html
 import secrets
+import threading
 from wsgiref.simple_server import make_server, WSGIRequestHandler
 
 SECRET = secrets.token_hex()
+ELEMENTS = {'etome.css': None, 'etome.js': None}
 
-with open('etome.css', 'r', encoding='utf-8') as f:
-    STYLE = re.sub(r'\s+', ' ', f.read())
+for _elem in ELEMENTS:
+    with open(os.path.join(DIR, _elem), 'r', encoding='utf-8') as f:
+        ELEMENTS[_elem] = f.read()
 
 
 class EmitHandler(WSGIRequestHandler):
@@ -166,16 +175,25 @@ class EmitHandler(WSGIRequestHandler):
 
 def request_facade(environ, respond):
     layers = [p for p in re.split(r'[/]+', environ['PATH_INFO']) if p]
-    depth, status, bodyplan = len(layers), '200 OK', None
+    method = environ['REQUEST_METHOD']
+    depth, status, bodyplan, title = len(layers), '200 OK', None, None
     prelude = visitor_prelude(environ)
-    if depth >= 1:
+    if depth == 0:
+        bodyplan = index_facade(environ)
+    if depth == 1:
         topic = layers[0]
-        artifact = palace_recall(topic)
-        bodyplan = artifact[0][2] if artifact else None
+        artifact = palace_recall(topic, fetch='last')	
+        title = f'<h3>Latest {topic}</h3>'
+        bodyplan = html.escape(artifact[0][2]) if artifact else None
+    elif depth == 2:
+        topic, aspect = layers
+        if aspect == '--':
+            title = f'<h3>Create {topic}</h3>'
+            bodyplan = artifact_form(topic)
     if bodyplan is None:
         status = '404 Not Found'
         bodyplan = f'Not found: {layers=}'
-    hypertext = hyperlink_text(prelude, bodyplan)
+    hypertext = hyperlink_text(prelude, title, bodyplan)
     headers = [('Content-type', f'text/html; charset=utf-8')]
     respond(status, headers)
     return [hypertext.encode('utf-8')]
@@ -185,41 +203,75 @@ def visitor_prelude(environ):
     visitor = environ['REMOTE_ADDR']
     palace_recall('visitors', store=visitor)
     topics = " ".join(f'[{t}]' for t in TOPICS)
-    top = f"Hi {visitor}, welcome to QACZAR.\n\tTopics: {topics}"
+    top = (
+        f"Hi <mark>{visitor}</mark>, welcome to "
+        f"<h1><a href='/'>{SITE}</a>!</h1> {topics}")
     return top
 
 
-def delegation_facade(environ, delegate):
-    s = create_fork(f'{HOST}:{PORT}', delegate)
-    emit(f"Delegate {s.pid=} {delegate=}")
-    watch_over(s)
+def index_facade(environ):
+    return f"""<p>{SITE} is a <a href="https://en.wikipedia.org/wiki/Quine_(computing)">Quine</a>"""
 
 
 def hyperlink_text(*artifacts):
     parts = []
     for artifact in artifacts:
+        if artifact is None:
+            continue
         for topic in TOPICS:
             artifact = re.sub(
                 rf'\b{topic}\b', f'<a href="/{topic}">{topic}</a>', artifact)
         indented = re.sub('    ', '&nbsp;&nbsp;&nbsp;&nbsp;', artifact)
         verses = indented.split('\n')
-        for i, verse in enumerate(verses):
-            if verse.startswith('#'):
-                verses[i] = f'<span class="notes"># {verse[1:]}</span>'
-        part = '\n'.join(f'<li>{verse}\n</li>' for verse in verses)
+        if len(verses) > 1:
+            for i, verse in enumerate(verses):
+                if verse.startswith('#'):
+                    verses[i] = f'<span class="notes"># {verse[1:]}</span>'
+            part = ''.join(f'<li>{verse}</li>' for verse in verses)
+        else:
+            part = f'<p>{indented}</p>'
+        # Put <ol> around parts if there is a <li> in it.
+        if '<li>' in part:
+            part = f'<ol>{part}</ol>'
         parts.append(part)
-    style = adapted_style()
-    body = parts[0] if len(parts) == 1 else '<hr>\n'.join(parts)
-    hypertext = f'<!DOCTYPE html>{style}<body><ol>{body}</ol></body></html>'
-    return hypertext
+    styles, scripts, hr = adapted_styles(), adapted_scripts(), 'hr'
+    body = parts[0] if len(parts) == 1 else f'<{hr}>\n'.join(parts)
+    return f'<!DOCTYPE html>{styles}<body><main>{body}</main></body>{scripts}</html>'
 
 
-def adapted_style():
+def adapted_styles():
     return f"""
         <link rel="stylesheet" media="screen" 
             href="https://fontlibrary.org//face/press-start-2p" type="text/css"/> 
-        <style>{STYLE}</style>
+        <style>{ELEMENTS['etome.css']}</style>
     """
+
+def adapted_scripts():
+    return f"""
+        <script>{ELEMENTS['etome.js']}</script>
+    """
+
+
+def watch_elements():
+    global ELEMENTS
+    elems = {f: os.path.getmtime(f) for f in ELEMENTS.keys()}
+    while True:
+        for f, mtime in elems.items():
+            if os.path.getmtime(f) > mtime:
+                with open(f, 'r', encoding='utf-8') as e:
+                    ELEMENTS[f] = re.sub(r'\s+', ' ', e.read())
+                emit(f'Reloaded {f=}.')
+                elems[f] = os.path.getmtime(f)
+        time.sleep(2)
+
+
+def artifact_form(topic):
+    return f"""
+        <form action="/{topic}/--" method="post">
+            <textarea name="artifact" rows="10" cols="80"></textarea>
+            <input type="submit" value="Submit">
+        </form>
+    """.strip()
 
 
 # TODO: Add 2 functions to generate and handle forms.
@@ -230,6 +282,7 @@ if __name__ == "__main__" and RUNLEVEL == 2:
     HOST, PORT = sys.argv[1].split(':')
     PORT = int(PORT)
     palace_recall('bodyplan', store=BODY)
+    threading.Thread(target=watch_elements).start()
     with make_server(HOST, PORT, request_facade, handler_class=EmitHandler) as s:
         emit(f'Facade ready at http://{HOST}:{PORT}/')
         s.serve_forever(poll_interval=1)
