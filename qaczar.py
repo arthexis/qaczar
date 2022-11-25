df75d33c4e1ef2151e023a3e779f95711f2e4a14
Rafael Guillén <arthexis@gmail.com>
2022-11-25 00:05:45 -0600

    Automatic commit by certify_build.

--- qaczar.py
+++ qaczar.py
@@ -162,7 +162,7 @@ def hyper(text, wrap=None):
         if isinstance(text, bytes): yield text
         elif isinstance(text, str): 
             if '\n' in text:
-                yield from (hyper(f'<li>{line}</li', 'ol') for line in text.splitlines())
+                yield from (hyper(f'<li>{line}</li>', 'ol') for line in text.splitlines())
             else: yield text.encode('utf-8')
         elif isinstance(text, Article): yield from hyper(text.article)
         elif isinstance(text, (list, tuple)): 
