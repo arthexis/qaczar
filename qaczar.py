ff5060466c725140f7a50386c38be0719a6a96dc
Rafael Guillén <arthexis@gmail.com>
2022-11-27 13:31:50 -0600

    Automatic commit by certify_build.

--- qaczar.py
+++ qaczar.py
@@ -198,7 +198,7 @@ def palace_recall(topic, /, fetch=True, store=None):
         emit(f'Palace error {e=} {sql=}'); raise
     c.close()
 
-TopicSummary = collections.namedtuple('TopicSummary', 'topic ver ts summary')
+TopicSummary = collections.namedtuple('TopicSummary', 'topic fname ver ts summary')
 
 def palace_summary():
     global PALACE
@@ -208,7 +208,7 @@ def palace_summary():
     for t in c.fetchall():
         found = c.execute(f'SELECT MAX(ver), MAX(ts), SUBSTR(content, 0, 54) '
             f'FROM {t[0]} GROUP BY ts ORDER BY ts DESC ').fetchone()
-        if found: yield TopicSummary(t[0].replace('__', '.'), *found)
+        if found: yield TopicSummary(t[0], t[0].replace('__', '.'), *found)
     c.close()
 
 
