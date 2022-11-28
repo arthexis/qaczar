4d9746b0efbf8895fd67b831b2dac1f221e8c67e
Rafael Guillén <arthexis@gmail.com>
2022-11-27 22:49:46 -0600

    Commit by certify_build.

--- qaczar.py
+++ qaczar.py
@@ -306,7 +306,7 @@ def article_combinator(articles):
     if not articles:
         # This is the overview page, when no topic is specified.
         th = {'Topic': 'a', 'Ver': None, 'Timestamp': 'time', 
-                'Size': None, 'Content Type': None}	
+                'Size': None, 'Content Type': 'q'}	
         g = (x for x in format_table(th, palace_summary(), 'Palace Summary'))
         yield from hyper(g, wrap='article')
         articles = {palace_recall('roadmap.txt')}
