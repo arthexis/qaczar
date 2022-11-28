4696b24bf30b94f78fcb47ff2478333f11e88ce7
Rafael Guillén <arthexis@gmail.com>
2022-11-27 21:15:14 -0600

    Commit by certify_build.

--- qaczar.py
+++ qaczar.py
@@ -465,7 +465,6 @@ def certify_build():
     emit(f'Certification complete at {isotime()}')
     
 
-            
 # TODO: Think about how to deploy to AWS after SSL is working.            
 # TODO: Think of new functions to add to qaczar.
 
