aac423e4a77437a88c1344f50520b7864916e316
Rafael Guillén <arthexis@gmail.com>
2022-11-28 12:22:11 -0600

    Commit by certify_build.

--- qaczar.py
+++ qaczar.py
@@ -352,6 +352,7 @@ def html_doc_stream(articles, form):
     # TODO: Use accesskey="#" and number the links.
     links = []  
     yield from hyper('<!DOCTYPE html><head><meta charset="utf-8"/>')
+    yield from hyper('<meta http-equiv="refresh" content="6"/>')
     yield from hyper(SITE, wrap='title')  
     if css: yield from hyper(css.content, 'style')  
     yield from hyper('</head><body><nav>')   
