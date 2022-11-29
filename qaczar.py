c2372db6e4df29faccfa1eb488a27eadfd17d9e8
Rafael Guillén <arthexis@gmail.com>
2022-11-28 23:45:12 -0600

    Commit by self_check.

--- qaczar.py
+++ qaczar.py
@@ -292,7 +292,6 @@ def linkify_topics(text):
     return ''.join(result)
 
 def format_python_line(line):
-    # TODO: Python line formatting still doesn't look right.
     line = line.replace('  ', '&nbsp;').replace('\t', '&nbsp;&nbsp;')
     line = linkify_topics(line)
     if line.strip().startswith('#') or line.strip().startswith('emit('):
