aecd906652a183cdc80d6dfad025096a6e78439f
Rafael Guillén <arthexis@gmail.com>
2022-11-28 19:13:02 -0600

    Commit by self_check.

--- qaczar.py
+++ qaczar.py
@@ -338,7 +338,7 @@ def process_forms(env, topic):
             if q.startswith('!'):
                 report = q[1:].replace(' ', '_') + '__html'
                 content, form, redirect = process_query(q[1:], topic)
-                if report: palace_recall(report, store=content)
+                if report and content: palace_recall(report, store=content, append=True)
                 if redirect: return None, redirect
                 msg = (f"Completed query='{q}' on {topic=}. "
                     f"Report: <a href='{report}'>{report}</a>.")
