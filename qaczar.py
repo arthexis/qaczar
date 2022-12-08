830348499467bfb2f858791c3388743a4d92151d
Rafael Guillén <arthexis@gmail.com>
2022-12-07 20:19:56 -0600

    running python in script tags

--- qaczar.py
+++ qaczar.py
@@ -4,43 +4,44 @@
 # A Python script designed to experiment with rapid application development.
 # H. V. D. C. by Rafa Guillén (arthexis@gmail.com) 2022-2023
 
-# 1. Keep the line width to less than 100 characters.
-# 2. Use functions, not classes, for modularity, composability and encapsulation.
-# 3. Functions should not reference functions or globals from later in the script.
-# 4. The system must respond to all requests in 1 second or less.
-# 5. Don't overdesign, wait until the opportunity for reuse arises and take it.
-# 6. Accomplish everything in under 4000 lines of code.
-# 7. Prioritize stability over features.
+# - Keep the line width to less than 100 characters.
+# - Use functions, not classes, for modularity, composability and encapsulation.
+# - Functions should not reference functions or globals from later in the script.
+# - Prioritize stability and clarity over features.
 
 import os
 import sys
 import time
-import socket
 import atexit
 import typing as t
 import subprocess as sp
+import xml.dom.minidom as dom
 
 
 RUNLEVEL = 0
 
 
-# General purpose functions.
-
-def isotime() -> str: 
+def isotime() -> str:  # Time in UTC ISO format.
     return time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())
 
 def emit(msg: str) -> None: 
-    f = sys._getframe(1)
+    f = sys._getframe(1)  # Caller's frame.
     print(f'[{RUNLEVEL}:{f.f_lineno}] [{isotime()}] {f.f_code.co_name}: {msg}')
 
-def terminate(msg: str, code: int=1) -> t.NoReturn:
-    emit(msg + " Terminating."); sys.exit(code)
+def halt(msg: str, code: int=1) -> t.NoReturn:
+    emit(msg + " Halting."); sys.exit(code)
 
-def read_bytes(fname: str) -> bytes:
-    with open(fname, 'rb') as f: return f.read()
+def read_bytes(fname: str, encoding=None) -> bytes:
+    with open(fname, 'rb' if not encoding else 'r', encoding=encoding) as f: return f.read()
     
-def write_bytes(fname: str, data: bytes) -> None:
-    with open(fname, 'wb') as f: f.write(data)
+def write_bytes(fname: str, data: bytes | str, encoding=None) -> None:
+    with open(fname, 'wb' if not encoding else 'w', encoding=encoding) as f: 
+        f.write(data)
+        f.flush()
+
+def get_mtime(fname: str) -> float:
+    return os.path.getmtime(fname)
+
 
 def start_script(*args: list[str]) -> sp.Popen:
     s = sp.Popen([sys.executable, *[str(a) for a in args]])
@@ -55,61 +56,86 @@ def restart_script(s: sp.Popen = None) -> sp.Popen:
     args = s.args + (os.getpid(),) if s.args[0] == __file__ else args
     return start_script(*args)
 
+
 # Watch over s to ensure it never dies. If it does, create a new one.
 def watch_over(s: sp.Popen, fname: str) -> t.NoReturn:  
-    source, mtime, stable = read_bytes(fname), os.path.getmtime(fname), True
+    source, mtime, stable = read_bytes(fname), get_mtime(fname), True
     while True:
         time.sleep(2.6)
-        if os.path.getmtime(fname) != mtime:
-            mutation, mtime = read_bytes(fname), os.path.getmtime(fname)
+        if (_mtime := get_mtime(fname)) != mtime:
+            mutation, mtime = read_bytes(fname), _mtime
             if mutation != source:
                 emit(f"Mutation detected. Restart {fname=}.")
-                # Make the new mutation start with a new main.
                 s, stable = restart_script(s), False
             continue
         if s.poll() is not None:
             if s.returncode == 0:
-                terminate(f"Process {s.pid} exited normally.")
+                halt(f"Process {s.pid} exited normally.")
             if stable:
-                emit(f"Fork died {s.args=} {s.pid=}. Restarting.")
+                emit(f"Script died {s.args=} {s.pid=}. Restarting.")
                 s, stable = restart_script(s), False
                 continue
             if (mutation := read_bytes(fname)) != source:
                 emit(f"Rolling back {fname=}.")
                 write_bytes(fname, source)
-                s, stable = restart_script(s), False
+                s = restart_script(s)
                 continue
-            else:
-                if mutation != source: write_bytes(fname, mutation)
-                terminate(f"Unstable {fname=}. Check source for errors.")
+            halt(f"Unstable {fname=}. Check source.")
         if not stable:
             emit(f"Stabilizing {s.pid=}.")
             source, stable = read_bytes(fname), True
 
+            
 def watch_self(watcher=None):
     if watcher: os.kill(int(watcher), 9)
     watch_over(start_script(__file__, __file__), __file__)
 
-def list_files():
-    return [f for f in os.listdir() if not f.startswith(('__', '.'))]
 
-def main_loop(pid=None, *args: list[str]) -> t.NoReturn:
-    emit(f"Starting {pid=} {args=}.")
-    if pid is not None: 
-        emit(f"Kill watcher {pid=}. Activate self watch.")
-        watch_self(watcher=pid)
-    while True:
-        # TODO: Decide what to do here.
-        emit(f"Looping {pid=} {args=}.")
-        # Get a list of all the files in the current directory.
-        # Exclude files that start with __ or .
-        files = [f for f in os.listdir() if not f.startswith(('__', '.'))]
-        emit(f"Files: {files}")
-        time.sleep(6.0)
+def process_html(fname: str) -> None:
+    start = time.time()
+    document = dom.parseString(read_bytes(fname, encoding='utf-8'))
+    for node in document.getElementsByTagName('script'):
+        if node.getAttribute('type') == 'text/python':
+            emit(f"Executing {fname=} {node.nodeName=} {node.getAttribute('type')=}.")
+            code = node.firstChild.data
+            indent = len(code) - len(code.lstrip()) - 1
+            code = '\n'.join(line[indent:] for line in code.splitlines())
+            exec(code, globals(), locals())
+            node.parentNode.removeChild(node)   
+    write_bytes(os.path.join('.work', fname), document.toxml(), encoding='utf-8')
+    emit(f"Processed {fname=} in {time.time() - start:.2f} seconds.")
 
 # Start a wsgi server that serves the current directory.
-def start_server():
-    pass
+def serve_forever(host, port) -> t.NoReturn:
+    import http.server as hs
+    import socketserver as ss
+    # Create a work directory for preprocessed files.
+    
+    if not os.path.exists('.work'): os.mkdir('.work')
+    cache = {}  # Type: dict[str, (bytes, float)]
+    class Handler(hs.SimpleHTTPRequestHandler):
+        def log_message(self, format, *args):
+            emit(f"{self.address_string()} {format % args}")
+        def do_GET(self) -> None:
+            if self.path[1] in '._': return self.send_error(404)
+            ext = self.path[1:].split('.')[-1] if '.' in self.path else None
+            if ext == 'html':
+                process_html(self.path[1:])
+                self.path = '/.work' + self.path
+                emit(f"Preprocessed {self.path=}.")
+            return super().do_GET()
+    with ss.TCPServer((host, int(port)), Handler) as httpd:
+        source = read_bytes(__file__, encoding='utf-8')
+        emit(f"Serving at http://{host}:{port}")
+        httpd.serve_forever()
+
+# Main loop that starts at the very end of the script.
+def main_loop(pid:str=None, server:str=None, *args: list[str]) -> t.NoReturn:
+    emit(f"Starting {pid=} {args=}.")
+    if pid is not None:
+        emit(f"Kill watcher {pid=}. Become self-watcher.")
+        watch_self(watcher=pid)
+    serve_forever('localhost', 8080)
 
 # Begin script execution in watch mode.
 if __name__ == "__main__":
@@ -120,5 +146,3 @@ if __name__ == "__main__":
         RUNLEVEL = 2
         main_loop(*sys.argv[2:])
     watch_over(start_script(*sys.argv[1:]), sys.argv[1])
-
-        
