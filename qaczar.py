0d83fd8dba038d7607f9bedc5472ff96e988b807
Rafael Guillén <arthexis@gmail.com>
2022-12-11 18:28:08 -0600

    fixed serving from workpath

--- qaczar.py
+++ qaczar.py
@@ -25,33 +25,36 @@ import typing as t
 
 
 PID = os.getpid()
+REDLINE = 0
 
 def iso8601() -> str: 
     return time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())
 
 def emit(msg: str, _at=None) -> None: 
-    global PID
+    global PID, REDLINE
     f = _at or sys._getframe(1)  
+    if f.f_lineno > REDLINE: REDLINE = f.f_lineno
     print(f'[{PID}:{f.f_lineno} {iso8601()}] {f.f_code.co_name}: {msg}', file=sys.stderr)
 
 def halt(msg: str) -> t.NoReturn:
     f = sys._getframe(1)
     # TODO: Consider additional cleanup or information gathering.
-    emit(f"{msg} <- Final message.", _at=f); 
+    emit(f"{msg} <- Final message.", _at=f)
     emit(f"Halting all processes.", _at=f)
     sys.exit(0)
-
-def read_file(fn: str, encoding=None) -> bytes:
-    # TODO: See if we need to standardize on UTF-8.
-    with open(fn, 'rb' if not encoding else 'r', encoding=encoding) as f: return f.read()
-    
-def write_file(fn: str, data: bytes | str, encoding=None) -> None:
-    if encoding and not isinstance(data, str): data = str(data)
-    with open(fn, 'wb' if not encoding else 'w', encoding=encoding) as f: f.write(data)
 
 def mtime_file(fn: str) -> float:
     if not os.path.isfile(fn): return 0.0
     return os.path.getmtime(fn)
+
+def read_file(fn: str, encoding=None) -> bytes:
+    # TODO: Keep a cache of file contents to avoid reading the same file twice.
+    with open(fn, 'rb' if not encoding else 'r', encoding=encoding) as f: return f.read()
+    
+def write_file(fn: str, data: bytes | str, encoding=None) -> None:
+    if encoding and not isinstance(data, str): data = str(data)
+    # TODO: Consider using a temporary file and then renaming it to avoid data loss.
+    with open(fn, 'wb' if not encoding else 'w', encoding=encoding) as f: f.write(data)
 
 
 #@# 2. META-PROGRAMMING
@@ -74,25 +77,42 @@ def timed(f: t.Callable) -> t.Callable:
     @functools.wraps(f)
     def timed(*args, **kwargs):
         start = time.time(); elapsed = time.time() - start
+        emit(f"Function <{f.__name__}> {args=} {kwargs=} called.")
         result = f(*args, **kwargs)
         emit(f"Function <{f.__name__}> {args=} {kwargs=} took {elapsed:.4f} seconds.")
         return result
     return timed
 
-def extract_todos(fname: str) -> list[str]:
-    # TODO: Generalize to extract any comments.
-    # TODO: Extract constributions (how much a function is doing).
+def extract_todos(tag: str = 'TODO', fname: str='qaczar.py') -> list[str]:
+    # TODO: Measure extract contributions (how much a function is doing).
     todos = []
     for line in read_file(fname, encoding='utf-8').splitlines():
-        if (todo := line.lstrip().split('#', 1)[0].strip()) and todo.startswith('TODO:'):
+        if (todo := line.lstrip().split('#', 1)[0].strip()) and todo.startswith(f'{tag}:'):
             todos.append(todo[5:].strip())
     return todos
+
+def extract_function(func: str, fname: str='qaczar.py') -> str:
+    source = read_file(fname, encoding='utf-8')
+    lines, start, indent_level = [], 0, None
+    for i, line in enumerate(source.splitlines()):
+        if line.lstrip().startswith(f'def {func}('):
+            start = i + 1; indent_level = len(line) - len(line.lstrip())
+        elif start and len(line) - len(line.lstrip()) <= indent_level: break
+        elif start: lines.append(line)
+    return '\n'.join(lines)
+
+def first_diff(source: str, target: str) -> int | None:
+    for i, (s, t) in enumerate(zip(source, target)):
+        if s != t and not (s.isspace() and t.isspace()): return i
+    return None
 
 
 #@# 2. SUBPROCESSING
 
 import atexit
 import subprocess 
+
+PYTHON = sys.executable
 
 def arg_line(*args: tuple[str], **kwargs: dict) -> tuple[str]:
     for k, v in kwargs.items(): args += (f'--{k}={str(v)}', )
@@ -107,11 +127,12 @@ def split_arg_line(args: list[str]) -> tuple[tuple, dict]:
     return tuple(largs), kwargs
 
 def start_py(script: str, *args: list[str], **kwargs: dict) -> subprocess.Popen:
+    global PYTHON
     line_args = [str(a) for a in arg_line(*args, **kwargs)]
-    emit(f"Starting script='{os.path.basename(script)}' {line_args=}.")
+    emit(f"Starting {script=} {line_args=}.")
     # Popen is a context manager, but we want to keep proc alive and not wait for it.
     # We cannot use run() for this. Remember to manually terminate the process later.
-    proc = subprocess.Popen([sys.executable, script, *line_args],
+    proc = subprocess.Popen([PYTHON, script, *line_args],
                             stdout=sys.stdout, stderr=sys.stderr)
     proc._args, proc._kwargs = args, kwargs  # Magic for restart_python.
     atexit.register(proc.terminate)
@@ -124,52 +145,39 @@ def stop_py(proc: subprocess.Popen) -> tuple[tuple, dict]:
     atexit.unregister(proc.terminate)
     return proc._args, proc._kwargs
 
-def restart_py(proc: subprocess.Popen = None, opid=PID) -> tuple[subprocess.Popen, bool]:
+def restart_py(proc: subprocess.Popen = None, opid=PID) -> subprocess.Popen:
     if proc and proc.poll() is None: 
         args, kwargs = stop_py(proc)
         kwargs['opid'] = opid
     else: args, kwargs = [], {}
-    return start_py(__file__, *args, **kwargs)
-
-def linear_distance(source: str, mutation: str) -> int:
-    distance = 0
-    for line in source.splitlines():
-        if line not in mutation: distance += 1
-        else: mutation = mutation.replace(line, '', 1)
-    return distance - len(mutation)
+    return start_py('qaczar.py', *args, **kwargs)
 
 def watch_over(proc: subprocess.Popen, fn: str) -> t.NoReturn:  
-    # TODO: Restart the watcher after it has restarted the server.
+    # TODO: Perhaps detect changes to watcher code itself and stop in those cases.
+    global REDLINE
+    assert isinstance(proc, subprocess.Popen)
     source, old_mtime, stable = read_file(fn), mtime_file(fn), True
     while True:
         time.sleep(2.6)
         if (new_mtime := mtime_file(fn)) != old_mtime:
             mutation, old_mtime = read_file(fn), new_mtime
             if mutation != source:
+                # TODO: Add logic to detect if the mutation is a comment.
+                # TODO: Check if the mutation occurrs above the REDLINE. If so, mark unsafe?
                 emit(f"Mutation detected. Optimistic restart.")
-                proc, stable = restart_py(proc), False
+                proc, stable = restart_py(proc), True
             continue
-        if proc.poll() is not None:
-            if proc.returncode == 0:
-                sys.exit(0)
+        if proc.poll() is not None:  # Script terminated.
+            if proc.returncode == 0: sys.exit(0)  # Halt from below.
             if stable:
                 emit(f"Script died {proc.returncode=}. Restart and mark unstable.")
                 proc, stable = restart_py(proc), False
                 continue
-            if (mutation := read_file(fn)) != source :
-                if linear_distance(source, mutation) == 0:
-                    emit(f"No mutation detected. Optimistic restart.")
-                    proc, stable = restart_py(proc), False
-                    continue
-                else:
-                    emit(f"Mutation detected. Restart and mark unstable.")
-                    proc, stable = restart_py(proc), True
-                    continue
-            halt(f"Unstability detected. Please check source.")
+            halt(f"Script died twice. Stopping watcher.")
         if not stable:
             emit(f"Stabilizing {proc.pid=}.")
             source, stable = read_file(fn), True
-
+            
 
 #@# 4. CONTENT GENERATION
 
@@ -177,11 +185,11 @@ import io
 import xml.dom.minidom as dom
 from contextlib import redirect_stdout
 
-WORKDIR = os.path.join(os.path.dirname(__file__), '.work')
+WORKDIR = os.path.join(os.path.dirname('qaczar.py'), '.work')
 
 def set_workdir(role: str='work') -> None:
     global WORKDIR
-    WORKDIR = os.path.join(os.path.dirname(__file__), f'.{role}')
+    WORKDIR = os.path.join(os.path.dirname('qaczar.py'), f'.{role}')
 
 def work_path(fname: str) -> str:
     global WORKDIR
@@ -200,24 +208,51 @@ def exec_inline(source: str, *, context: dict) -> str:
         exec(source, None, context)
         return stdout.getvalue()
 
+def process_py(fname: str, context: dict) -> str:
+    # TODO: Function fragments are not being extracted properly.
+    emit(f"Processing {fname=} with {context=}.")
+    if '@' in fname: 
+        func_name, script = fname.split('@')
+        module_name = script[:-3]
+        # Import the module and get the function.
+        module = importlib.import_module(module_name)
+        fname = f"{func_name}@{module_name}.html"
+        emit(f"Calling {func_name=} in module='{module_name} {fname=}.")
+        if context['method'] == 'POST':
+            function = getattr(module, func_name, None)
+            content = function(**context['formdata'])
+        else:
+            content = extract_function(func_name, fname=script)
+            emit(f"Extracted {len(content)=} bytes from {func_name=} in {script=}.")
+    else:
+        content = read_file(fname, encoding='utf-8')
+        if context['method'] == 'POST':
+            content = exec_inline(content, context=context['formdata'])
+    write_file(wp := work_path(fname), content, encoding='utf-8')
+    emit(f"Written to {wp=} as {fname=} ({len(content)=} bytes).")
+    return wp
+
 def process_html(fname: str, context: dict) -> str:
     # TODO: New function to automatically create a form from a function.
-    document = dom.parseString(read_file(fname, encoding='utf-8'))
+    # TODO: Extract and process variables in the form of ${var}.
+    source = read_file(fname, encoding='utf-8')
+    document = dom.parseString(source)
     context['document'] = document
-    for node in iter_scripts(document):
-        new_html = exec_inline(dedent(node.firstChild.data), context=context)
-        replace_node(old=node, new=new_html)
+    # Loop over every text node and replace variables.
+    for node in document.getElementsByTagName('body')[0].childNodes:
+        if node.nodeType == node.TEXT_NODE and '{' in node.data:
+            node.data = node.data.format(**os.environ, **globals(), **locals(), **context)
     xml = document.toxml()
     write_file(wp := work_path(fname), xml[xml.index('?>') + 2:], encoding='utf-8')
+    emit(f"Written to {wp=} as {fname=} ({len(xml)=} bytes).")  
     return wp
     
 @timed
-def dispatch_processor(fname: str, context: dict) -> str:
+def dispatch_processor(fname: str, context: dict) -> str | None:
     if (processor := globals().get(f'process_{fname.split(".")[-1]}')):
         emit(f"Processing {fname=} with <{processor.__name__}>.")
-        processor(fname, context)
-    return f'/{fname}'
-
+        return processor(fname, context)
+    return None
 
 #@# 5. COMMUNICATION 
 
@@ -248,7 +283,7 @@ def pip_import(module: str) -> t.Any:
     except ModuleNotFoundError:
         emit(f"Installing {module=}.")
         if '.' in module: module = module.split('.')[0]
-        sp.run([sys.executable, '-m', 'pip', 'install', module])
+        subprocess.run([sys.executable, '-m', 'pip', 'install', module])
         return importlib.import_module(module)
     
 def imports(*modules: tuple[str]) -> t.Callable:
@@ -312,17 +347,21 @@ def build_https_server() -> tuple:
         def build_context(self, path:str, method: str = None) -> dict:
             if '?' not in path: qs = ''
             else: path, qs = path.split('?', 1)
-            context = {'ip': self.client_address[0], 'ts': iso8601()}
-            context['qs'] = parse.parse_qs(qs)
-            if method == 'POST': context['post'] = parse.parse_qs(self.rfile_read())
-            else: context['post'] = {}
+            context = {'ip': self.client_address[0], 'ts': iso8601(), 'method': method}
+            context['query'] = parse.parse_qs(qs)
+            if method == 'POST': context['formdata'] = parse.parse_qs(self.rfile_read())
+            else: context['formdata'] = {}
             return context
 
         def build_response(self, method: str = None) -> bool:
             # TODO: Change default response depending on the next role of the server.
-            path = '/qaczar.html' if self.path == '/' else self.path
-            context = self.build_context(path, method)
-            self.path = dispatch_processor(path[1:], context)
+            self.path = '/qaczar.html' if self.path == '/' else self.path
+            context = self.build_context(self.path, method)
+            self.work_path = dispatch_processor(self.path[1:], context)
+            emit(f"{context['ip']} {context['ts']} {method} {self.path} ({self.work_path})")
+            
+        def translate_path(self, path: str = None) -> str:
+            return super().translate_path(path) if not self.work_path else self.work_path
 
         def do_HEAD(self) -> None:
             self.build_response('HEAD'); return super().do_HEAD()
@@ -358,15 +397,16 @@ def client_request(
 #@# 6. COMMON ROLES
 # NOTE: All <role>_loop functions must have *args and **kwargs as parameters.
 
-def watcher_loop(*args, next: str = None, **kwargs) -> t.NoReturn:
+def watcher_role(*args, next: str = None, **kwargs) -> t.NoReturn:
     # TODO: Include a mode to watch external processes.
+    # TODO: This could later be used to gather the output of the process being watched.
     # The watcher itself is started in a deamon thread. IF this thread dies, the main
     # thread will start a new watcher with the same param.
-    if not next: raise ValueError('next role is not defined')
+    if not next: raise ValueError('next role was not defined')
     kwargs['role'] = next
-    watch_over(start_py(__file__, *args, **kwargs), __file__)
+    watch_over(start_py('qaczar.py', *args, **kwargs), 'qaczar.py')
 
-def server_loop(*args, host='localhost', port='9443', **kwargs) -> t.NoReturn:
+def server_role(*args, host='localhost', port='9443', **kwargs) -> t.NoReturn:
     # TODO: After the server is ready, figure out what to start next (not the tester).
     # TODO: See if we can use doctest to test the code of _loop functions.
     # TODO: Should we pass extra arguments to the server? (e.g. for the relay)
@@ -374,21 +414,38 @@ def server_loop(*args, host='localhost', port='9443', **kwargs) -> t.NoReturn:
     with server_cls((host, int(port)), handler_cls) as httpd:
         emit(f"Server ready at https://{host}:{port}")
         atexit.register(httpd.shutdown)
-        time.sleep(2)
         httpd.serve_forever()
 
-def tester_loop(*args, role: str = None, **kwargs) -> t.NoReturn:
+def tester_role(*args, role: str = None, **kwargs) -> t.NoReturn:
     # TODO: After tests are completed successfully, restart the opid watcher.
+    # TODO: Consider some kind of chaos monkey testing.
     # doctest.testmod(verbose=True)
     emit(f"Tester done.")
-    
-def relay_loop(*args, **kwargs: dict) -> t.NoReturn:
+
+def messenger_role(*args, **kwargs: dict) -> t.NoReturn:
     # TODO: Listener that queues requests and forwards them to the server.
     # TODO: Multiple input channels should be supported.
     raise NotImplementedError
 
-def chaos_loop(*args, **kwargs: dict) -> t.NoReturn:
+def monitor_role(*args, **kwargs: dict) -> t.NoReturn:
     raise NotImplementedError
+
+def role_dispatcher(role: str, args: tuple, kwargs: dict) -> None:
+    # TODO: Roles with __ prefix are reserved for internal use.
+    # TODO: If the launched role has a test_<role> function, run it.
+    import threading
+    emit(f"Assuming role='{__role}' args={__args} kwargs={__kwargs}")
+    opid = kwargs.pop('opid', None)  # If we receive opid it means we are being watched.
+    def dispatch():
+        try:
+            function = globals().get(f"{role}_role")
+            if function is None: raise ValueError(f"Role '{role}' is not defined.")
+            function(*args, **kwargs)
+        except Exception as e:
+            emit(f"Exception in role '{role}': {type(e)}: {e}")
+            raise
+    threading.Thread(target=dispatch, daemon=True).start()
+    while threading.active_count() > 1: time.sleep(60)
 
 
 if __name__ == "__main__":
@@ -396,23 +453,19 @@ if __name__ == "__main__":
         __role, __args, __kwargs = 'watcher', [], {'next': 'server'}
     else:
         __args, __kwargs = split_arg_line(sys.argv[1:])
-        __role = __kwargs.pop('role')  # This should never fail.
-    emit(f"In role='{__role}' args={__args} kwargs={__kwargs}")
-    # TODO: Roles with __ prefix are reserved for internal use.
-    # TODO: If the launched role has a test_<role> function, run it.
+        __role = __kwargs.pop('role')  # It's ok to fail if role is not defined.
     set_workdir(__role)
-    try:
-        locals()[f"{__role}_loop"](*__args, **__kwargs)
-    except Exception as e:
-        emit(f"Exception in role '{__role}': {type(e)}: {e}")
-         
-    # tester_loop(role=__role, args=__args, kwargs=__kwargs)
+    DEBUG = True if 'debug' in __args else DEBUG
+    role_dispatcher(__role, __args, __kwargs)
 
 
 #@# 7. TESTS
 # NOTE: All test_<role> functions must have *args and **kwargs as parameters.
 
 # TODO: Implement our own blockchain for versioning and testing.
+
+def hello_world() -> str:
+    return "Hello World!"
 
 def test_server_loop(requests, *args, **kwargs) -> t.NoReturn:
     # See if we can reach the server.
