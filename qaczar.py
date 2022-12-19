bc675eb6e3d5781e050c1863f6f1f329f6877dd0
Rafael Guillén <arthexis@gmail.com>
2022-12-18 19:44:24 -0600

    auto commit

--- qaczar.py
+++ qaczar.py
@@ -286,6 +286,7 @@ def process_py(fname: str, context: dict) -> str:
 @timed
 def _dispatch_processor(fname: str, context: dict) -> str | None:
     if '.' not in fname: 
+        prefix, suffix = f'{fname}/{fname}', 'html'
         raise NotImplementedError(f"Directory listing not implemented for {fname}")
     prefix, suffix = fname.split(".", 1)  # Only one dot is allowed.
     if '/' in suffix: 
