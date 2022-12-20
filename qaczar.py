8e9f54087a7fe3433c69125b5714775e4b204974
Rafael Guillén <arthexis@gmail.com>
2022-12-20 13:11:28 -0600

    auto commit

--- qaczar.py
+++ qaczar.py
@@ -571,7 +571,7 @@ def _build_handler() -> type:
         def send_header(self, keyword: str, value: str) -> None:
             if keyword == 'Content-Type' and 'text' in value and 'encoding' not in value:
                 value = f"{value}; charset=utf-8"
-            elif keyword == 'Server': value = f"{value} ({APP})"
+            elif keyword == 'Server': value = f"{value} (qaczar.py)"
             emit(f"Sent header {keyword}: {value}")
             return super().send_header(keyword, value)
 
