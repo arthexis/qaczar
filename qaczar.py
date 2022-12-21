985ae94f52d77558de7aac56656338eb9eb2576c
Rafael Guillén <arthexis@gmail.com>
2022-12-20 21:58:31 -0600

    auto commit

--- qaczar.py
+++ qaczar.py
@@ -409,6 +409,8 @@ def _dispatch_processor(fname: str, context: dict) -> str | None:
         context['subpath'] = subpath
     if (processor := globals().get(f'process_{suffix}')):
         try:
+            # TODO: Fix template error: TypeError: unhashable type: 'list'
+            # This error happens when using a query string with a form.
             return processor(f'{prefix}.{suffix}', context)
         except Exception as e:
             emit(f"Error processing {fname}: {e}")
