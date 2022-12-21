26cf518363ad8dd3ecb927ec0b46f398f0c96de0
Rafael Guillén <arthexis@gmail.com>
2022-12-20 20:35:39 -0600

    auto commit

--- qaczar.py
+++ qaczar.py
@@ -596,7 +596,7 @@ def _build_handler() -> type:
         def _build_response(self, method: str = None) -> bool:
             # COM EDU GOV NET ORG
             self.start = time.time()
-            if self.path.endswith('/'): self.path = f'{self.path}/{APP}.html'
+            if self.path.endswith('/'): self.path = f'{self.path}{APP}.html'
             if '?' not in self.path: path, qs = self.path, ''
             else: path, qs = self.path.split('?', 1)
             emit(f"Request from {self.client_address[0]}: {method} {path} {qs}")
