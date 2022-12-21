c3c590150765e6db312cf2e7e5b21d249756e5a3
R. J. Guillén-Osorio <arthexis@gmail.com>
2022-12-21 11:57:45 -0600

    auto commit

--- qaczar.py
+++ qaczar.py
@@ -251,7 +251,7 @@ _BASE_HTML = """
         <h2>Roadmap</h2>
         <ol class="ln">${q.enum_file('qaczar.py', prefix='# TODO:')}</ol><hr />
         <!-- ERROR -->
-        ${q.render_form('sign_guestbook')}
+        ${q.render_form(q.sign_guestbook)}
         
     </%block>
     <%block name="footer">
