76ab9f2c9d435a63c905de3d3847091a00d85ea2
R. J. Guillén-Osorio <arthexis@gmail.com>
2022-12-22 16:06:10 -0600

    auto commit

--- qaczar.py
+++ qaczar.py
@@ -153,7 +153,7 @@ def _setup_environ() -> None:
 def _start_py(script_path: str, *args: list[str], **kwargs: dict) -> subprocess.Popen:
     global _PYTHON
     line_args = [str(a) for a in _arg_line(*args, **kwargs)]
-    emit(f"Starting {script_path=} {line_args=}.")
+    # emit(f"Starting {script_path=} {line_args=}.")
     # Popen is a context manager, but we want to keep proc alive and not wait for it.
     # We cannot use run() for this. Remember to manually terminate the process later.
     proc = subprocess.Popen([_PYTHON, script_path, *line_args],
