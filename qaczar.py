1c58c1577866da38baf0b38f2642948500fa502d
R. J. Guillén-Osorio <arthexis@gmail.com>
2022-12-22 22:04:51 -0600

    auto commit

--- qaczar.py
+++ qaczar.py
@@ -404,11 +404,15 @@ def build_html_chain(*func_names: str, **context) -> str:
 
 #@# WEB COMPONENTS
 
-@hyper('body')
+@hyper('body', ('header', 'main', 'footer'))
 def hello_world(**qs) -> str:
     """Let this be the default page. It shall have a roadmap.""" 	
     # TODO: Add all the missing elements (such as the roadmap) to the welcome page.
-    return elem('h1', f"QACZAR")
+    return [
+            elem('h1', f"QACZAR"),
+            elem('h2', f"Welcome to {APP}!"),
+            elem('p', f"Here's a roadmap of what's coming up next."),
+        ]
 
 
 #@# HTTPS SERVER
