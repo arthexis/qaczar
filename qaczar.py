f3da73e53e9bdadfce7ab7ffca9b2fa0efa176f0
R. J. Guillén-Osorio <arthexis@gmail.com>
2022-12-26 09:42:40 -0600

    auto commit

--- qaczar.py
+++ qaczar.py
@@ -446,6 +446,7 @@ def site_endpoints() -> t.List[str]:
 @hyper('nav')
 def site_nav(title: str = None) -> str:
     """Let this be the header of the each page on the site."""
+    # TODO: Fix the CSS, because this overlaps with the body.
     global _INDEX, SITE
     links = [elem('a', (title or '').upper(), href=page) for page in site_endpoints()]
     return elem_h1(title or SITE, href='/'), *links
